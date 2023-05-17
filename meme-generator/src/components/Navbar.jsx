2dc9f3122db0254bdcdf7f037e837ca57b988435
Eldar Heleg <eldar.heleg@gmail.com>
2023-05-17 20:51:32 +0200

    navbar

--- meme-generator/src/components/Navbar.jsx
+++ meme-generator/src/components/Navbar.jsx
@@ -1,1 +1,13 @@
-f
+import React from 'react'
+
+function Navbar() {
+  return (
+      <nav className='navbar'>
+          <img className='navbar--picture' src="/images/troll-face.png" alt="Meme face" />
+          <h2 className='navbar--title'>Meme generator</h2>
+          <h3 className="navbar--second--title">Project 3 - Meme generator</h3>
+    </nav>
+  )
+}
+
+export default Navbar
