b8b4707b7149871968018363587f15482ccab483
Eldar Heleg <eldar.heleg@gmail.com>
2023-05-18 18:07:17 +0200

    generate component styling

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Generate from "./components/Generate";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Generate/>
     </div>
   );
 }
---
+++ meme-generator/src/components/Generate.jsx
@@ -0,0 +1,11 @@
+function Generate() {
+  return (
+    <section className="main">
+      <input type="text" className="main--upperText" />
+      <input type="text" className="main--lowerText" />
+      <button className="main--generate-button">Get a new image 😎</button>
+    </section>
+  );
+}
+
+export default Generate;
