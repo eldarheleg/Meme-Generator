b2ffb9cd10a2aa6c71f09bb05516fbe685cba73c
Eldar Heleg <eldar.heleg@gmail.com>
2023-05-19 11:31:58 +0200

    added data

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Generate from "./components/Generate";
 import Navbar from "./components/Navbar";
 
+
 function App() {
   return (
     <div className="App">
--- meme-generator/src/components/Generate.jsx
+++ meme-generator/src/components/Generate.jsx
@@ -1,9 +1,22 @@
+import memesData from "../memesData";
+
 function Generate() {
+  function getRanImg() {
+    const memesArr = memesData.data.memes;
+    const randomNum = Math.floor(Math.random() * memesArr.length);
+    console.log(memesArr[randomNum].url);
+  }
   return (
     <section className="main">
-      <input type="text" className="main--upperText" />
-      <input type="text" className="main--lowerText" />
-      <button className="main--generate-button">Get a new image 😎</button>
+      <input placeholder="Top Text" type="text" className="main--upperText" />
+      <input
+        placeholder="Bottom Text"
+        type="text"
+        className="main--lowerText"
+      />
+      <button onClick={getRanImg} className="main--generate-button">
+        Get a new image 😎
+      </button>
     </section>
   );
 }
