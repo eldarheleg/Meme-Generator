7226883ae032276f754a3560a9636763d2a62be8
Eldar Heleg <eldar.heleg@gmail.com>
2023-05-25 22:27:15 +0200

    showing generated images

--- meme-generator/src/components/Generate.jsx
+++ meme-generator/src/components/Generate.jsx
@@ -1,23 +1,30 @@
+import React from "react";
 import memesData from "../memesData";
 
 function Generate() {
+  const [memeImg, setMemeImg] = React.useState("");
   function getRanImg() {
     const memesArr = memesData.data.memes;
     const randomNum = Math.floor(Math.random() * memesArr.length);
+
+    setMemeImg(memesArr[randomNum].url);
     console.log(memesArr[randomNum].url);
   }
   return (
-    <section className="main">
-      <input placeholder="Top Text" type="text" className="main--upperText" />
-      <input
-        placeholder="Bottom Text"
-        type="text"
-        className="main--lowerText"
-      />
-      <button onClick={getRanImg} className="main--generate-button">
-        Get a new image 😎
-      </button>
-    </section>
+    <main>
+      <div className="main">
+        <input placeholder="Top Text" type="text" className="main--upperText" />
+        <input
+          placeholder="Bottom Text"
+          type="text"
+          className="main--lowerText"
+        />
+        <button onClick={getRanImg} className="main--generate-button">
+          Get a new image 😎
+        </button>
+      </div>
+      <img src={memeImg} alt="img" className="generated--img" />
+    </main>
   );
 }
 
