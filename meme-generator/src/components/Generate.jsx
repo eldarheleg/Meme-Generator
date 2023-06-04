467d3c11d9d5cc77f2d15e6a86e8eefee3938c5f
Eldar Heleg <eldar.heleg@gmail.com>
2023-06-04 19:53:59 +0200

    styling text on image

--- meme-generator/src/components/Generate.jsx
+++ meme-generator/src/components/Generate.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import memesData from "../memesData";
 
 function Generate() {
-  const [memeImg, setMemeImg] = React.useState("");
-  function getRanImg() {
-    const memesArr = memesData.data.memes;
-    const randomNum = Math.floor(Math.random() * memesArr.length);
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/gk5el.jpg",
+  });
 
-    setMemeImg(memesArr[randomNum].url);
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getRanImg() {
+    const memesArr = allMemeImages.data.memes;
+    const randomNum = Math.floor(Math.random() * memesArr.length);
+    let url = memesArr[randomNum].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
     console.log(memesArr[randomNum].url);
   }
   return (
@@ -23,7 +33,11 @@ function Generate() {
           Get a new image 😎
         </button>
       </div>
-      <img src={memeImg} alt="img" className="generated--img" />
+      <div className="main--final">
+        <img src={meme.randomImage} alt="img" className="generated--img" />
+        <h2 className="main--text top">something1</h2>
+        <h2 className="main--text bottom">something2</h2>
+      </div>
     </main>
   );
 }
